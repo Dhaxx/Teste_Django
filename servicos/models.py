58d65229cbe847f91dcbf61e32ed53b13404776f
Dhaxx <kaiopablo44@gmail.com>
2023-12-20 23:58:22 -0300

    Estilizar Expositor Reduzido de Serviços

--- servicos/models.py
+++ servicos/models.py
@@ -7,6 +7,7 @@ class Servicos(models.Model):
     titulo = models.CharField(max_length=100, null=False, blank=False)
     descricao = models.CharField(max_length=50, null=True, blank=True)
     imagem = models.ImageField(upload_to=upload_path, null=True, blank=True)
+    visivel = models.BooleanField(default=True)
     
     def __str__(self):
         return self.titulo
