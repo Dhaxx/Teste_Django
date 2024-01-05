f69d551917db584765d8f86b3fed5c58729b4a12
Dhaxx <kaiopablo44@gmail.com>
2024-01-04 22:58:27 -0300

    Finalizando seção do Projeto Detalhado

--- projetos/views.py
+++ projetos/views.py
@@ -8,9 +8,9 @@ def expositor_projetos(request):
 
 def projetos(request, tipo_projeto=None):
     if tipo_projeto:
-        projetos = Projetos.objects.filter(tipo=tipo_projeto)
+        projetos = Projetos.objects.filter(tipo=tipo_projeto, visivel=True)
     else:
-        projetos = Projetos.objects.all()
+        projetos = Projetos.objects.filter(visivel=True)
 
     paginacao = Paginator(projetos, 6)
     page_num = request.GET.get('page')
@@ -20,6 +20,7 @@ def projetos(request, tipo_projeto=None):
 
 def projeto_detalhado(request, titulo):
     projeto = Projetos.objects.get(titulo=titulo)
+    projetos = Projetos.objects.all()
     carrosseis_do_projeto = projeto.carrossel_set.all()
 
     imagens_do_projeto = []
@@ -29,4 +30,4 @@ def projeto_detalhado(request, titulo):
         
         imagens_do_projeto.extend(imagens_do_carrossel)
 
-    return render(request, 'projetos/projeto_detalhado.html', {'projeto': projeto, 'carrossel': carrosseis_do_projeto, 'imagens_do_projeto':imagens_do_projeto})
+    return render(request, 'projetos/projeto_detalhado.html', {'projeto': projeto, 'carrossel': carrosseis_do_projeto, 'imagens_do_projeto':imagens_do_projeto, 'projetos':projetos})
