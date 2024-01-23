34d670e663bc68dbe5f4383098b168f6a65f3f5d
Dhaxx <kaiopablo44@gmail.com>
2024-01-22 21:42:51 -0300

    Finalizando projeto para telas maiores

--- contato/views.py
+++ contato/views.py
@@ -3,8 +3,20 @@ from .forms import ContatoForm
 
 # Create your views here.
 def contato_form(request):
-    form = ContatoForm()
-    context = {
-        'form': form
-    }
-    return render(request, 'contato/contato.html', context=context)
+    if request.method == 'GET':
+        form = ContatoForm()
+        context = {
+            'form': form
+        }
+        return render(request, 'contato/contato.html', context=context)
+    else:
+        form = ContatoForm(request.POST)
+        if form.is_valid():
+            contato = form.save()
+            form = ContatoForm()
+            
+        context = {
+            'form': form
+        }
+        return render(request, 'contato/contato.html', context=context)
+        
